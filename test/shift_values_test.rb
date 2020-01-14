a9e945fa549368e6867228a372252b0c545e89a3
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 07:09:19 -0700

    create key values test, test fails

--- test/shift_values_test.rb
+++ test/shift_values_test.rb
@@ -13,4 +13,8 @@ class ShiftValuesTest < Minitest::Test
   def test_squared
     assert_equal 14428814400, @shift.squared("120120")
   end
+
+  def test_key_values
+    assert_equal [12, 23, 34, 45], @shift.key_values("12345")
+  end
 end
