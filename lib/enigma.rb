1dd3f5392879b74dd8ba6385ae64038bba403f0c
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 12:31:05 -0700

    create encrypt method with inheritance, test passes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,4 +9,8 @@ class Enigma < Cypher
   def random_num
     rand(10**5).to_s.rjust(5, '0')
   end
+
+  def encrypt(input, key = random_num, date = current_date)
+    Cypher.encode(input, key, date)
+  end
 end
