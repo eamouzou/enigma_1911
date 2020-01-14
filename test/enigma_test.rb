30bb3f9092eb880e75e348b08bc3d4aaa6fc2596
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 12:17:24 -0700

    create random num test, test fails

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,4 +9,9 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_random_number_method
+    assert_equal 5, @enigma.random_num.length
+    assert_equal String, @enigma.random_num.class
+  end
 end
