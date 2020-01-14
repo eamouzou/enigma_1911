e46dabf9a2c6de97eac5d53b7ac74491e95c86ec
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 12:39:04 -0700

    create decrypt without a date test, test passes

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -47,4 +47,14 @@ class EnigmaTest < Minitest::Test
     }
     assert_equal expected, @enigma.encrypt("hello world", "02715")
   end
+
+  def test_it_can_decrypt_without_a_date
+    @enigma.stubs(:current_date).returns("040895")
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715")
+  end
 end
