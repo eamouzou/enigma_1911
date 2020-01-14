51b28e991632187c13c0112b593107f5cd49001a
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 12:23:14 -0700

    create current date test, test fails

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,6 +10,11 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_current_date
+    assert_equal String, @enigma.current_date.class
+    assert_equal 6, @enigma.current_date.length
+  end
+
   def test_random_number_method
     assert_equal 5, @enigma.random_num.length
     assert_equal String, @enigma.random_num.class
