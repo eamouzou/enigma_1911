75a4a88f424d3409c7d1a1e1de66ed07c20aa7f3
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 08:06:30 -0700

    create alphabet class method test, test fails

--- test/cypher_test.rb
+++ test/cypher_test.rb
@@ -5,4 +5,10 @@ class CypherTest < Minitest::Test
   def test_inherits_values
     assert_equal [3, 27, 73, 20], Cypher.values("02715", "040895")
   end
+
+  def test_creates_alphabet
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, Cypher.alphabet
+  end
 end
