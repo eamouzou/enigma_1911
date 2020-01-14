4ac5f6e456f179216e46b18b51c56c6986791b02
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 08:13:26 -0700

    create encode matched alphabet class method test, test fails

--- test/cypher_test.rb
+++ test/cypher_test.rb
@@ -11,4 +11,37 @@ class CypherTest < Minitest::Test
       "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, Cypher.alphabet
   end
+
+  def test_encode_matched_alphabet
+    expected = {"a"=>"e",
+      "b"=>"f",
+      "c"=>"g",
+      "d"=>"h",
+      "e"=>"i",
+      "f"=>"j",
+      "g"=>"k",
+      "h"=>"l",
+      "i"=>"m",
+      "j"=>"n",
+      "k"=>"o",
+      "l"=>"p",
+      "m"=>"q",
+      "n"=>"r",
+      "o"=>"s",
+      "p"=>"t",
+      "q"=>"u",
+      "r"=>"v",
+      "s"=>"w",
+      "t"=>"x",
+      "u"=>"y",
+      "v"=>"z",
+      "w"=>" ",
+      "x"=>"a",
+      "y"=>"b",
+      "z"=>"c",
+      " "=>"d"}
+      
+      assert_equal expected, Cypher.encode_matched_alphabet([3, 27, 73, 20])
+  end
+
 end
