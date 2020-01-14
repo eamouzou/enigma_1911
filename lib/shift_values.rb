09e24f4e5463699e6477553f7f4f9503408a8377
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 07:17:10 -0700

    create offset values method, test passes

--- lib/shift_values.rb
+++ lib/shift_values.rb
@@ -5,6 +5,16 @@ class ShiftValues
   end
 
   def key_values(num)
-    num.chars.each_cons(2).map {|number| numbers.join.to_i}
+    num.chars.each_cons(2).map {|numbers| numbers.join.to_i}
   end
+
+  def offset_values(date)
+    squared(date).to_s.chars.last(4).map {|number| number.to_i}
+  end
+
+  # def values(num, date)
+  #   key = key_values(num)
+  #   offset = offset_values(date)
+  #   key.zip(offset).map {|values| values.sum}
+  # end
 end
